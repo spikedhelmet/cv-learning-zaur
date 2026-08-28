383c027858f7ec4fb9b3feadb006cc83748a7437
zaur3 <zaur@jis.com>
2026-08-28 18:24:30 +0400

    script fix

--- nutivo/crop_products.py
+++ nutivo/crop_products.py
@@ -11,8 +11,6 @@ for shelf_index,result in enumerate(results, start=1):
     crop_counter = 0
     img = result.orig_img
     img_height, img_width = img.shape[:2]
-    if img_width < 20 or img_height < 20:
-        continue
 
     boxes_xyxy = result.boxes.xyxy
     conf_numbers = result.boxes.conf.cpu().numpy()
@@ -28,7 +26,10 @@ for shelf_index,result in enumerate(results, start=1):
         modded_x2 = min(img_width, int(x2))
         modded_y1 = max(0, int(y1))
         modded_y2 = min(img_height, int(y2))
-        # print("Coords:", x_1,x_2, y_1, y_2)
+
+        if (modded_x2 - modded_x1) < 20 or (modded_y2 - modded_y1) < 20:
+            continue
+
         cropped_img = img[modded_y1:modded_y2, modded_x1:modded_x2]
 
         # 1. Format the string using f-strings with zero-padding (:02d and :03d)

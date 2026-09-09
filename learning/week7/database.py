79f686d960d993b342ea122f7793aed986270b50
zaur3 <zaur@jis.com>
2026-09-09 16:51:10 +0400

    database.py ready

---
+++ learning/week7/database.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+connection = None
+cursor = None
+
+def init_db():
+    global connection, cursor
+    connection = sqlite3.connect("learning/week7/detections.db", check_same_thread=False)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+
+    sql_statement = """
+    CREATE TABLE IF NOT EXISTS events (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp TEXT,
+        frame INTEGER,
+        track_id INTEGER,
+        class_name TEXT,
+        confidence REAL,
+        zone_intrusion BOOLEAN    
+    );
+    """
+
+    cursor.execute(sql_statement)
+    connection.commit()
+
+    return connection
+
+def log_event(timestamp, frame, track_id, class_name, confidence, zone_intrusion):
+    cursor.execute(
+    "INSERT INTO events (timestamp, frame, track_id, class_name, confidence, zone_intrusion) VALUES (?, ?, ?, ?, ?, ?)",
+    (timestamp, frame, track_id, class_name, confidence, zone_intrusion)
+    )
+    connection.commit()
+
+def get_recent_events(limit):
+    cursor.execute("SELECT * FROM events ORDER BY id DESC LIMIT ?", (limit,))
+    rows = cursor.fetchall()  # Returns a list of tuples
+    
+    return rows
+    
+
+# Temporary test block - delete this after running!
+if __name__ == "__main__":
+    print("Initializing DB...")
+    init_db()
+    
+    print("Logging a test event...")
+    log_event("2026-09-09T16:30:00", 120, 1, "drone", 0.95, True)
+    
+    print("Fetching recent events:")
+    events = get_recent_events(5)
+    for event in events:
+        print(dict(event)) # dict() converts the sqlite3.Row into a readable dictionary

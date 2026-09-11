fe830dd95cba753e382ca34ff12d7b286b3a6d5e
zaur3 <zaur@jis.com>
2026-09-11 17:37:34 +0400

    backend continue

--- learning/week7/server.py
+++ learning/week7/server.py
@@ -1,3 +1,5 @@
+from fastapi import WebSocketDisconnect
+from fastapi import WebSocket
 from fastapi.responses import StreamingResponse
 import cv2
 import asyncio
@@ -86,3 +88,46 @@ async def video_feed():
         generate_frames(),
         media_type="multipart/x-mixed-replace; boundary=frame"
     )
+
+@app.websocket("/ws/detections")
+async def ws_endpoint(websocket:WebSocket):
+    await websocket.accept()
+    print("Browser connected to websocket")
+
+    last_frame_number = -1
+
+    try:
+        while True:
+            with state_lock:
+                frame_num = shared_state["frame_number"]
+                detections = shared_state["detections"]
+                intrusions = shared_state["intrusion_count"]
+
+            # Only send if new frame
+            if frame_num != last_frame_number:
+                last_frame_number = frame_num
+
+                await websocket.send_json({
+                    "frame_number":frame_num,
+                    "intrusion_count":intrusions,
+                    "detections": detections,
+                })
+                
+            # Wait 10ms before checking for new data
+            await asyncio.sleep(0.01)
+
+    except WebSocketDisconnect:
+        print("Browser disconnected from WebSocket")
+
+@app.get("/api/zones")
+async def get_zones():
+    return
+
+@app.post("/api/zones")
+async def post_zones(name,x1,y1,x2,y2):
+    return
+
+@app.delete("/api/zones/{zone_name}")
+async def post_zones(name):
+    return
+
